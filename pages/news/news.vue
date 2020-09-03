e7eedd8c95784a78b635feb74f0e0f71fa7dc60a
王水胜 <602466011@qq.com>
2020-09-03 16:40:24 +0800

    更新: 路由命名

---
+++ pages/news/news.vue
@@ -0,0 +1,32 @@
+<template>
+  <view>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+			requestParams: {
+				pageSize: 10,
+			},
+			list: []
+		}
+	},
+	created() {
+		this.requestParams.time = new Date().getTime() + ''
+	},
+  methods: {
+		getData() {
+			return new Promise((resolve, reject) => {
+				uni.request({
+					url: 'https://unidemo.dcloud.net.cn/api/news'
+				})
+			})
+		}
+  }
+}
+</script>
+
+<style>
+</style>
