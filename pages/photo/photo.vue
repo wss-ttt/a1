8de1552797182e22cf5ddaaaddfff47d5db9472d
王水胜 <602466011@qq.com>
2020-09-04 10:34:12 +0800

    更新: 测试下拉刷新效果

--- pages/photo/photo.vue
+++ pages/photo/photo.vue
@@ -1,22 +1,41 @@
 <template>
-	<view>
-		photo页
-	</view>
+  <view class="wrapper">
+    <div v-for="(item, index) in list" :key="index">好好学习 {{ item }}</div>
+  </view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			}
-		},
-		methods: {
-			
-		}
-	}
+export default {
+  data() {
+    return {
+      list: []
+    }
+  },
+  onPullDownRefresh() {
+    this.loadData()
+  },
+  methods: {
+    loadData() {
+      setTimeout(() => {
+        for (let i = 0; i < 5; i++) {
+          this.list.unshift(i)
+        }
+        // 数据请求完成之后停止下拉刷新
+        uni.stopPullDownRefresh()
+      }, 1000)
+    }
+  }
+}
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.wrapper {
+  height: 100%;
+  overflow: auto;
+  background: #ccc;
+}
+uni-page-body,
+uni-page-refresh {
+  height: 100%;
+}
 </style>
