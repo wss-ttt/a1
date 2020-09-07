dd40a4283c203d3bd600dc229bbc4c3cb4be9cc5
王水胜 <602466011@qq.com>
2020-09-07 16:21:58 +0800

    添加: set

---
+++ pages/set/set.vue
@@ -0,0 +1,41 @@
+<template>
+	<view class="wrapper">
+		<list-cell title="个人资料"></list-cell>
+		<list-cell title="收获地址"></list-cell>
+
+		<view class="log-out">
+			退出登录
+		</view>
+	</view>
+</template>
+
+<script>
+	import ListCell from '@/components/list-cell/list-cell';
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+.wrapper {
+	.content {
+		background: #fff;
+	}	
+	.log-out {
+		margin-top: 40upx;
+		font-size: 32upx;
+		color: red;
+		@include center();
+		background: #fff;
+		padding: 20upx 0;
+	}
+}
+
+</style>
