6541985495103942eb1f52867bdd75bbe1bd4dd6
王水胜 <602466011@qq.com>
2020-09-07 16:39:39 +0800

    更新: set.vue 添加退出操作

--- pages/set/set.vue
+++ pages/set/set.vue
@@ -1,41 +1,47 @@
 <template>
-	<view class="wrapper">
-		<list-cell title="个人资料"></list-cell>
-		<list-cell title="收获地址"></list-cell>
+  <view class="wrapper">
+    <list-cell title="个人资料"></list-cell>
+    <list-cell title="收获地址"></list-cell>
 
-		<view class="log-out">
-			退出登录
-		</view>
-	</view>
+    <view class="log-out" @click="logOut">退出登录</view>
+  </view>
 </template>
 
 <script>
-	import ListCell from '@/components/list-cell/list-cell';
-	export default {
-		data() {
-			return {
-				
-			}
-		},
-		methods: {
-			
-		}
-	}
+import ListCell from '@/components/list-cell/list-cell'
+export default {
+  data() {
+    return {}
+  },
+  methods: {
+    logOut() {
+      uni.showModal({
+        content: '确定要退出登录吗',
+        success: res => {
+          if (res.concfirm) {
+            setTimeout(() => {
+              uni.navigateBack()
+            }, 200)
+          }
+        }
+      })
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
 .wrapper {
-	.content {
-		background: #fff;
-	}	
-	.log-out {
-		margin-top: 40upx;
-		font-size: 32upx;
-		color: red;
-		@include center();
-		background: #fff;
-		padding: 20upx 0;
-	}
+  .content {
+    background: #fff;
+  }
+  .log-out {
+    margin-top: 40upx;
+    font-size: 32upx;
+    color: red;
+    @include center();
+    background: #fff;
+    padding: 20upx 0;
+  }
 }
-
 </style>
