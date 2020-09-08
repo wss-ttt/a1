b26450c4b98db8c4ac0899ec1d47ca70c389a4c9
王水胜 <602466011@qq.com>
2020-09-08 12:47:55 +0800

    更新: login.vue

--- pages/login/login.vue
+++ pages/login/login.vue
@@ -1,18 +1,45 @@
 <template>
-  <view class="wrapper">
-  </view>
+  <div class="wrapper">
+    <view class="uni-common-mt">
+      <view class="uni-form-item uni-column">
+        <view class="title">可自动聚焦的input</view>
+        <input class="uni-input" focus placeholder="自动获得焦点" />
+      </view>
+      <view class="uni-form-item uni-column">
+        <view class="title">键盘右下角按钮显示为搜索</view>
+        <input class="uni-input" confirm-type="search" placeholder="键盘右下角按钮显示为搜索" />
+      </view>
+    </view>
+  </div>
 </template>
 
 <script>
 export default {
-  components: {
-    CmdInput
-  },
+  components: {},
   data() {
     return {
+      title: 'input',
+      focus: false,
+      inputValue: '',
+      changeValue: ''
     }
   },
-  methods: {}
+  methods: {
+    onKeyInput: function(event) {
+      this.inputValue = event.target.value
+    },
+    replaceInput: function(event) {
+      var value = event.target.value
+      if (value === '11') {
+        this.changeValue = '2'
+      }
+    },
+    hideKeyboard: function(event) {
+      if (event.target.value === '123') {
+        uni.hideKeyboard()
+      }
+    }
+  }
 }
 </script>
 
